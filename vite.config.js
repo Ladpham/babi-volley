e0d52e3e7cd2cb6ca447a916c72220e2598d5c8f
Ladpham <96309248+Ladpham@users.noreply.github.com>
2025-05-26 23:53:23 +0000

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/volleyball-app/' // ← your repo name
+});
