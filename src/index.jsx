83480b735b55aed5f7509af6a54a324c8d2d33d3
Ladpham <96309248+Ladpham@users.noreply.github.com>
2025-05-27 00:16:34 +0000

    Update and rename index.jsx to src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
-// File: src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import Admin from "./Admin.jsx";
+import App from "./App";
+import Admin from "./Admin";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
