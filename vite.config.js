e47ab66505e164c83ac0d7e28b21bc29b161aed6
Ladpham <96309248+Ladpham@users.noreply.github.com>
2025-05-27 00:34:10 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/babi-volley/' // adjust to your repo name
-})
+  base: '/babi-volley/' // 👈 must match your GitHub repo name
+});
